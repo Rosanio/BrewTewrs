6790d40594e1a4efc600d2198835b4f0edec6bf1
Andrew Telleria and Matt Rosanio <Guest@epicodus-74.local>
2016-05-10 11:56:45 -0700

    Start writing search activity

---
+++ BrewTewrs/app/src/main/java/com/epicodus/brewtewrs/ui/SearchActivity.java
@@ -0,0 +1,15 @@
+package com.epicodus.brewtewrs.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.epicodus.brewtewrs.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
